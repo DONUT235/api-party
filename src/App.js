d3422544284038f72e905791b7d859bf7215f7fa
Aaron Glave <glaveaf@rose-hulman.edu>
2018-06-18 10:40:29 -0400

    Display a param in /github/ path

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
+import { NavLink, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Github from './Github';
+
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<div class="App-header">
+				<div className="App-header">
 					<h3>Ain't no party like an </h3>
 					<h1>API Party</h1>
 				</div>
+				<ul className="navLinks">
+					<li>
+						<NavLink to="/github">Github API</NavLink>
+					</li>
+				</ul>
+
+				<Switch>
+					<Route path="/github" component={Github}/>
+					<Route render={()=>(
+						<p>To get started click an API link.</p>
+					)}/>
+				</Switch>
+			</div>
 		);
 	}
 }
