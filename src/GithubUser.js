d205a20e317ba74e41766dc9637feecad5556511
Aaron Glave <glaveaf@rose-hulman.edu>
2018-06-18 11:14:32 -0400

    Display more info about the Github user

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
 
 class GithubUser extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			user: {},
+		};
+	}
+	fetchUserData() {
+		fetch(`https://api.github.com/users/${this.props.match.params.username}`)
+			.then(response => response.json())
+			.then(user => this.setState({user: user}));
+	}
+	componentDidMount() {
+		this.fetchUserData();
+	}
+	componentDidUpdate(prevProps) {
+		if(prevProps.match.params.username != this.props.match.params.username) {
+			this.fetchUserData();
+		}
+	}
 	render() {
+		const user = this.state.user;
 		return (
-			<div className="GithubUser"><h1>Github User: {this.props.match.params.username}</h1></div>
+			<div className="GithubUser">
+				<img src={user.avatar_url} />
+				<a href={user.html_url} target="_blank">
+					<h2>Github User: {user.name} ({user.login})</h2>
+				</a>
+			</div>
 		);
 	}
 }
